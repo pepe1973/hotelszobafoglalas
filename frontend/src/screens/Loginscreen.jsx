097f23aea8c45d93c246c509cd12966bd52664ac
adam323233 <rekotyea32@gmail.com>
2024-12-06 10:08:21 +0100

    12.06 10:08

--- frontend/src/screens/Loginscreen.jsx
+++ frontend/src/screens/Loginscreen.jsx
@@ -1,54 +1,57 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 const Loginscreen = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  async function login() {
-    const user = {
-      email,
-      password,
-    };
-  
-    if{
-      const response = await fetch('http://localhost:5000/api/users/login', {
-        method: 'POST', // A HTTP metódus
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const login = async () => {
+      const response = await fetch("http://localhost:5000/api/users/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json', // A kérés fejlécének beállítása
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(user), // Az adatokat JSON formátumban küldjük
+        body: JSON.stringify({ email, password }),
       });
-  
-      const result = await response.json(); // A válasz JSON formátumban
+
+      const result = await response.json();
+
       console.log(result);
-    } catch (error) {
-      console.error('Login failed:', error);
-    }
-  }
+
+      if (response.ok) {
+        alert("Sikeres regisztráció!");
+      } else {
+        alert(result.msg);
+      }
+    };
+
+    login();
+  };
 
   return (
     <div>
-      <div className='row justify-content-center mt-5'>
-        <div className='col-md-5 mt-5'>
-          <div className='bs'>
+      <div className="row justify-content-center mt-5">
+        <div className="col-md-5 mt-5">
+          <div className="bs">
             <h2>Bejelentkezés</h2>
             <input
-              type='text'
-              className='form-control'
-              placeholder='email'
+              type="text"
+              className="form-control"
+              placeholder="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
             <input
-              type='password' 
-              className='form-control'
-              placeholder='password'
+              type="password"
+              className="form-control"
+              placeholder="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
             <br />
-            <button className='btn btn-primary' onClick={login}>
+            <button className="btn btn-primary" onClick={handleSubmit}>
               Login
             </button>
           </div>
