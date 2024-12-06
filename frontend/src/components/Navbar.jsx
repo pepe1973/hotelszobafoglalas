937e33965b502c13cde082bfca9114f78baac007
adam323232 <rekotyea32@gmail.com>
2024-12-06 12:23:42 +0100

    12.06. 12:23

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
+  const user = JSON.parse(localStorage.getItem('currentUser'));
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
@@ -20,16 +21,32 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="/register">
-                Regisztráció
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/login">
-                Belépés
-              </a>
-            </li>
+            {user ? (
+              <>
+                <div class="dropdown">
+                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    {user.name}
+                  </button>
+                  <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                    <li><a class="dropdown-item" href="#">Foglalások</a></li>
+                    <li><a class="dropdown-item" href="#">Kijelentkezés</a></li>
+                  </ul>
+                </div>
+              </>
+            ) : (
+              <>
+                <li className="nav-item active">
+                  <a className="nav-link" href="/register">
+                    Regisztráció
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="/login">
+                    Belépés
+                  </a>
+                </li>
+
+              </>)}
           </ul>
         </div>
       </div>
--- frontend/src/screens/Loginscreen.jsx
+++ frontend/src/screens/Loginscreen.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
+import Loader from '../components/Loader'
+import Error from '../components/Error'
 
 const Loginscreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const login = async () => {
+
+      
+
       const response = await fetch("http://localhost:5000/api/users/login", {
         method: "POST",
         headers: {
@@ -32,8 +39,10 @@ const Loginscreen = () => {
 
   return (
     <div>
+      {loading && (<Loader/>)}
       <div className="row justify-content-center mt-5">
         <div className="col-md-5 mt-5">
+          {error && (<Error message='Nem léteik ilyen felhasználó' />)}
           <div className="bs">
             <h2>Bejelentkezés</h2>
             <input
